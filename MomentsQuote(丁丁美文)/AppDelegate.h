fa810fff8ca74e49b5fcff8016424c503a177db8
jakciehoo <niat05ethjh1112@163.com>
2015-08-03 16:59:51 +0800

    Initial Commit

---
+++ MomentsQuote(丁丁美文)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MomentsQuote(丁丁美文)
+//
+//  Created by HooJackie on 15/8/3.
+//  Copyright (c) 2015年 jackieHoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MomentsQuote(丁丁美文)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MomentsQuote(丁丁美文)
+//
+//  Created by HooJackie on 15/8/3.
+//  Copyright (c) 2015年 jackieHoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
